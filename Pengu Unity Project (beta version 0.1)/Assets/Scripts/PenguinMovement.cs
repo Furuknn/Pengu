d3cf68f9b28a221bbc356b38c0396ad29ef57a60
Furkna <erbilginfurkan@gmail.com>
2023-11-16 22:20:25 +0300

    Jump fonksiyonu doğru çalışıyor

--- Pengu Unity Project (beta version 0.1)/Assets/Scripts/PenguinMovement.cs
+++ Pengu Unity Project (beta version 0.1)/Assets/Scripts/PenguinMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PenguinMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    private Rigidbody2D rigidBody2D;
+    [Range(1,10)]
+    public float jumpVelocity = 7f;
+
+    private void Awake()
     {
-        
+        rigidBody2D = transform.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody2D.velocity = Vector2.up * jumpVelocity;
+        }
     }
+
+    
+
 }
