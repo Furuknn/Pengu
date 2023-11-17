0412b7aa40972ed4283ceebc2dc2bb7fc3894b31
Furkna <erbilginfurkan@gmail.com>
2023-11-17 15:33:48 +0300

    Revert "Slide eklendi"
    This reverts commit 4b030b1f0972b5c4976f4a95db19cd595d36b874.

--- Pengu Unity Project (beta version 0.1)/Assets/Scripts/PenguinMovement.cs
+++ Pengu Unity Project (beta version 0.1)/Assets/Scripts/PenguinMovement.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PenguinMovement : MonoBehaviour
 {
-    [HideInInspector] public bool isPenguinJumping = false;
-
+    
     private Rigidbody2D rigidBody2D;
     private BoxCollider2D boxColl2D;
     private Animator anim;
@@ -26,29 +25,25 @@ public class PenguinMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rigidBody2D.velocity = Vector2.up * jumpVelocity;
-            isPenguinJumping = true;
         }
         else if (IsGrounded()==false && rigidBody2D.velocity.y>0)
         {
             anim.SetBool("IsLaunching", true);
-            isPenguinJumping = true;
         }
         else if (IsGrounded() == false && rigidBody2D.velocity.y < 7)
         {
             anim.SetBool("IsLaunching", false);
             anim.SetBool("IsFalling", true);
-            isPenguinJumping = true;
         }
         else if (IsGrounded() == true)
         {
             anim.SetBool("JumpEnded", true);
             anim.SetBool("IsFalling", false);
-            isPenguinJumping = false;
         }
         
     }
 
-    public bool IsGrounded()
+    private bool IsGrounded()
     {
         return Physics2D.BoxCast(boxColl2D.bounds.center, boxColl2D.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
